da7ac374822f2eed1bfbd592f3f6daebd96ea6f7
Otto-404 <otto.jr404@gmail.com>
2025-03-17 09:04:01 -0400

    "New version"

--- main.py
+++ main.py
@@ -23,38 +23,43 @@ Sensor_Distance = InfraredSensor(Port.S2)
 robo = DriveBase(Motor_Left, Motor_Right,wheel_diameter=55.5, axle_track=117)
 
 # Calcule o limiar de luz.
-Line = 6
-Floor = 25
+Line = 5
+Floor = 20
 Limit = (Line + Floor) / 2
 Distance_Upper = 30
 Distance_Lower = 10
 Limit_Obstacle = (Distance_Upper - Distance_Lower) / 2
 # Define a velocidade do robo a um valor x milimetros por segundo.
-Speed = 350
-Gain = 0.6 
+Speed = 200
+Gain = 8
+signal = 1
 
 # Loop para seguir a linha sem ponto de parada.
+preto = False
+anteriorPreto=False
 while True:
-    print(Sensor_Distance.distance())
+    print(Sensor_Color.reflection())
     # Calcula o desvio do limite.
     Distance = Sensor_Distance.distance()
     Light = Sensor_Color.reflection()
-    Curve = Light - Limit
+    Curve = Light - Limit * signal
+    if (Limit < 0):
+        preto = True
+    else:
+        preto = False
+    if (anteriorPreto !=)
     # Calcula a taxa de rotação
     Turn_Rate = Gain * Curve
     # Define a velocidade base e a taxa de rotação baseada nos cálculos anteriores
     robo.drive(Speed, Turn_Rate)
+    signal = signal*(-1)
     # if obstaculo
-    while Distance <= Distance_Upper and Distance >= Distance_Lower:
-        robo.stop()
-        Turn_Rate = (Gain * 13)
-        robo.turn(90)
-        robo.drive(Speed, Turn_Rate)
-        wait (1000)
-        break
+   # while Distance <= Distance_Upper and Distance >= Distance_Lower:
+   #     robo.stop()
+   #     Turn_Rate = (Gain * 13)
+   #     robo.turn(90)
+   #     robo.drive(Speed, Turn_Rate)
+   #     wait (1000)
+   #     break
 
     wait(10)
-
-    if Light > 70:
-        robo.stop()
-        break
