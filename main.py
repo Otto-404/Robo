842ef0794ad70b78177f78c85c53690fd9e9acf5
Otto-404 <otto.jr404@gmail.com>
2025-05-23 15:05:48 -0400

    "Robo-Gol"

--- main.py
+++ main.py
@@ -1,33 +1,16 @@
 #!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, ColorSensor,InfraredSensor)
+from pybricks.ev3devices import Motor
 from pybricks.parameters import Port
 from pybricks.tools import wait
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.robotics import DriveBase
-from pybricks.parameters import Port
-from time import sleep, time
-#Ports
-PORT_MOTOR_LEFT = Port.B
-PORT_MOTOR_RIGHT = Port.C
-PORT_SENSOR_COLOR = Port.S3
 
-# Var Colors
-LINE = 2
-FLOOR = 27
-LIMIT = (LINE + FLOOR) / 2
+# Ports
+PORT_MOTOR_LEFT = Port.D
+PORT_MOTOR_RIGHT = Port.A
 
-# VAR MOTORS
+# Configurações iniciais
 WHEEL_DIAMETER = 55.5
 AXEL_TRACK = 147
-TURN_CORRECTION = 1
-SPEED = 1000
-GAIN = 7
-DISTANCE_CORRECT = 1
-TURN_CORRECT_LEFT = 1
-TURN_CORRECT_RIGHT = 1
-
-MAX_TIME_IN_BLANK = 1000
 
 eve3 = EV3Brick()
 
@@ -35,117 +18,65 @@ def printValue(text: str):
     print(text)
     eve3.screen.clear()
     eve3.screen.draw_text(1, 1, text)
-    
-
-Sensor_Color = ColorSensor(PORT_SENSOR_COLOR)
 
 class Robo:
     def __init__(self):
-        Motor_Left = Motor(Port.B)
-        Motor_Right = Motor(Port.C)
-        Motor_Left.reset_angle(100)
-        self.motor = DriveBase(Motor_Left, Motor_Right,wheel_diameter=WHEEL_DIAMETER, axle_track=AXEL_TRACK)
-        self.motor.settings(straight_speed=5000, straight_acceleration=10000)
-    
-    def drive(self, speed, angle):
-        self.motor.drive(-speed, -angle)
+        # Motores individuais
+        self.motor_esquerdo = Motor(PORT_MOTOR_LEFT)
+        self.motor_direito = Motor(PORT_MOTOR_RIGHT)
 
-    def stop(self):
-        self.motor.stop()
+        # Zera ângulo inicial
+        self.motor_esquerdo.reset_angle(0)
+        self.motor_direito.reset_angle(0)
 
-    def straight(self, distancia):
-        self.motor.straight(-0.4481 * distancia) 
-    def turn(self, angulo):
-        angulo = -angulo
-        angulo_novo = 0
-        if (angulo > 0):
-            angulo_novo = 1.10092* angulo
-        else:
-            angulo_novo = 1.052632 * angulo
-        self.motor.turn(angulo_novo)
+    def mover_motor_esquerdo(self, velocidade, tempo_ms):
+        self.motor_esquerdo.run(velocidade)
+        wait(tempo_ms)
+        self.motor_esquerdo.stop()
 
+    def mover_motor_direito(self, velocidade, tempo_ms):
+        self.motor_direito.run(velocidade)
+        wait(tempo_ms)
+        self.motor_direito.stop()
 
-def inLine(light):
-    if light < LIMIT:
-        return print("Encontrei a linha preta")
-    else:
-        return print("Estou na linha branca")
-
+    def parar_todos(self):
+        self.motor_esquerdo.stop()
+        self.motor_direito.stop()
 
 robo = Robo()
-
-    
+var = 1
 while True:
-    #Start
-    robo.drive(SPEED,0)
-    wait(1900)
-    robo.drive(SPEED,75)
+    # Exemplo de uso:
+    if var == 1:
+        printValue("Movendo Cabeça")
+        robo.mover_motor_direito(380, 500)
+        wait(1000)
+        var = 2
+
+    printValue("Movendo Pernas")
+    robo.mover_motor_esquerdo(408.5, 450)
     wait(500)
-    robo.drive(SPEED,0)
-    wait(1500)
-    robo.drive(SPEED,-75)
-    wait(500)
-    robo.drive(SPEED,0)
-    wait(1600)
-    robo.drive(SPEED,-75)
-    wait(500)
-    robo.drive(SPEED,0)
-    wait(1600)
-    robo.drive(SPEED,76)
-    wait(500)
-    robo.drive(SPEED,0)
-    wait(1600)
-    #Checkpoint 1
-    robo.drive(SPEED,50)
-    wait(500)
-    robo.drive(SPEED,0)
+    printValue("Movendo Cabeça")
+    robo.mover_motor_direito(-600, 550)
     wait(700)
-    robo.drive(SPEED,-50)
+    printValue("Movendo Pernas")
+    robo.mover_motor_esquerdo(408.5, 450)
     wait(500)
-    robo.drive(SPEED,0)
-    wait(1650)
-    robo.drive(SPEED,-45)
-    wait(450)
-    robo.drive(SPEED,0)
-    wait(1150)
-    #curva S
-    robo.drive(SPEED,45)
-    wait(600)
-    robo.drive(SPEED,0)
-    wait(600)
-    robo.drive(SPEED,45)
-    wait(1100)
-    robo.drive(SPEED,0)
-    wait(900)
-    robo.drive(SPEED,-40)
-    wait(1500)
-    robo.drive(SPEED,0)
-    wait(2100)
-    robo.drive(SPEED,-75)
+    printValue("Movendo Cabeça")
+    robo.mover_motor_direito(600, 550)
+    wait(700)
+    printValue("Movendo Pernas")
+    robo.mover_motor_esquerdo(408.5, 450)
     wait(500)
-    #Checkpoint 2
-    robo.drive(SPEED,0)
-    wait(1200)
-    robo.drive(SPEED,70)
+    printValue("Movendo Cabeça")
+    robo.mover_motor_direito(-600, 550)
+    wait(700)
+    printValue("Movendo Pernas")
+    robo.mover_motor_esquerdo(420, 500)
     wait(500)
-    robo.drive(SPEED,0)
-    wait(800)
-    robo.drive(SPEED,75)
-    wait(500)
-    robo.drive(SPEED,0)
-    wait(1200)
-    robo.drive(SPEED,-70)
-    wait(500)
-    robo.drive(SPEED,0)
-    wait(500)
-    robo.drive(SPEED,-65)
-    wait(500)
-    robo.drive(SPEED,0)
-    wait(950)
-    robo.drive(SPEED,70)
-    wait(500)
-    robo.drive(SPEED,0)
-    wait(3500)
-    robo.stop
-    break
- 
+    printValue("Movendo Cabeça")
+    robo.mover_motor_direito(600, 550)
+    wait(700)
+
+    #printValue("Parando motores")
+   #robo.parar_todos()
